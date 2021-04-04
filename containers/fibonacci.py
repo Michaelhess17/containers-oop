532529f8212f3bd1e3dd9410504ac2a685d95c24
Michael Hess <michaelhess17@yahoo.com>
2021-04-03 23:57:56 -0700

    fix unicode

--- containers/fibonacci.py
+++ containers/fibonacci.py
@@ -64,7 +64,7 @@ class Fib:
         self.n = n
 
     def __iter__(self):
-       if self.n is not None:
+        if self.n is not None:
             return FibIter(self.n - 1)
         else:
             return FibIter()
@@ -80,28 +80,23 @@ class FibIter:
     '''
     This is the iterator helper class for the Fib class.
     '''
-<<<<<<< HEAD
-    def __init__(self, n):
+    def __init__(self, n=None):
         self.n = n
-        self.i = 0
         self.f0 = 1
         self.f1 = 1
+        self.ret = 0
+        self.i = 0
 
     def __next__(self):
-        if self.n is not None and self.i >= self.n:
+        if self.n is not None and self.n + 1 <= self.i:
             raise StopIteration
-        if self.i == 0:
-            self.i += 1
-            return 1
-        elif self.i == 1:
-            self.i += 1
-            return 1
         else:
-            self.i += 1
-            self.f2 = self.f0 + self.f1
+            tmp = self.f0
+            self.f2 = self.f1 + self.f0
             self.f0 = self.f1
             self.f1 = self.f2
-            return self.f2
+            self.i += 1
+            return tmp
 
 
 def fib_yield(n=None):
@@ -110,7 +105,7 @@ def fib_yield(n=None):
     first n fibonacci numbers.
     If n is None, then the generator is infinite.
     '''
-   if n is not None and n <= 2:
+    if n is not None and n <= 2:
         for _ in range(n):
             yield 1
     elif n is None:
--- containers/unicode.py
+++ containers/unicode.py
@@ -20,25 +20,29 @@ class NormalizedStr:
     '''
 
     def __init__(self, text, normal_form='NFC'):
-        pass
+        self.nf = normal_form
+        self.text = unicodedata.normalize(self.nf, text)
+        self.i = -1
 
     def __repr__(self):
         '''
         The string returned by the __repr__ function should be valid python code
         that can be substituted directly into the python interpreter to reproduce an equivalent object.
         '''
+        return "NormalizedStr(\'{}\', \'{}\')".format(self.text, self.nf)
 
     def __str__(self):
         '''
         This functions converts the NormalizedStr into a regular string object.
         The output is similar, but not exactly the same, as the __repr__ function.
         '''
-
+        return str(self.text) 
     def __len__(self):
         '''
         Returns the length of the string.
         The expression `len(a)` desugars to a.__len__().
         '''
+        return len(self.text)
 
     def __contains__(self, substr):
         '''
@@ -48,32 +52,39 @@ class NormalizedStr:
         HINT:
         You should normalize the `substr` variable to ensure that the comparison is done semantically and not syntactically.
         '''
+        norm_ss = unicodedata.normalize(self.nf, substr)
+        return str(self.text).__contains__(str(norm_ss))
 
     def __getitem__(self, index):
         '''
         Returns the character at position `index`.
         The expression `a[b]` desugars to `a.__getitem__(b)`.
         '''
-
+        return self.text.__getitem__(index)
+    
     def lower(self):
         '''
         Returns a copy in the same normalized form, but lower case.
         '''
+        return self.text.lower()
 
     def upper(self):
         '''
         Returns a copy in the same normalized form, but upper case.
         '''
+        return self.text.upper()
 
     def __add__(self, b):
         '''
         Returns a copy of `self` with `b` appended to the end.
         The expression `a + b` gets desugared into `a.__add__(b)`.
 
-        HINT:
+        HInT:
         The addition of two normalized strings is not guaranteed to stay normalized.
         Therefore, you must renormalize the strings after adding them together.
         '''
+        new_text = unicodedata.normalize(self.nf, self.text + unicodedata.normalize(self.nf, str(b)))
+        return NormalizedStr(new_text, self.nf)
 
     def __iter__(self):
         '''
@@ -82,3 +93,25 @@ class NormalizedStr:
         You'll need to define your own iterator class with the appropriate magic methods,
         and return an instance of that class here.
         '''
+        return self
+        # return NormStrIter(self.text)
+
+    def __next__(self):
+        if self.i == len(self.text) - 1:
+            raise StopIteration
+        else:
+            self.i += 1
+            return self.text[self.i]
+
+
+class NormStrIter:
+    def __init__(self, text):
+        self.text = text
+        self.i = -1
+
+    def __next__(self):
+        if self.i == len(self.text) - 1:
+            raise StopIteration
+        else:
+            self.i += 1
+            return self.text[self.i]
